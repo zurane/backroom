3b69bfc06dcafe6e832ffd8ff9a44ce6fcb73ea4
Mpho <giftedmpho99@gmail.com>
2024-08-13 15:05:11 +0200

    Created User Model and Schema

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+
+  .connect(process.env.MONGO) // This is to hide our URL connecting to our application database from the public in the env file.
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((error) => {
+    console.log("database connection unsuccessful: ", error);
+  });
 
 const app = express();
 
---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true } // For recording timestamps 
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
